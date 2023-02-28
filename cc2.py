b1c182c7ecd8145ddbe305a1e1a77cdd58605c53
Nasreen Noel Mbagwu <108826027+nasreen-noels@users.noreply.github.com>
2023-02-27 19:47:04 -0500

    Create cc2.py

---
+++ cc2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Analyze output from robot line following algorithim. 
+
+# 1. read commands from text file
+# 2. determine aciton from input parameter
+# 3. visualize robot path or summarize time spent moving/turning
